ed0df2feee2e77be8b8927481d3c227bfd96c255
billow <yaagnto@qq.com>
2019-04-02 21:23:00 +0800

    ok?

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,3 @@
-set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
-set -x PIPENV_PYPI_MIRROR https://pypi.tuna.tsinghua.edu.cn/simple
-
 abbr setclip "xclip -selection c"
 abbr getclip "xclip -selection c -o"
 
@@ -15,3 +12,8 @@ set -gx OCAML_TOPLEVEL_PATH "/home/billow/.opam/system/lib/toplevel";
 set -gx PERL5LIB "/home/billow/.opam/system/lib/perl5:$PERL5LIB";
 set -gx MANPATH "$MANPATH" "/home/billow/.opam/system/man";
 set -gx CAML_LD_LIBRARY_PATH "/home/billow/.opam/system/lib/stublibs:/nix/store/8h1jaraypcfb65xk6w1apjxdqyf2rq16-ocaml-4.06.1/lib/ocaml/stublibs";
+
+# Mirror
+set -gx PIPENV_PYPI_MIRROR https://pypi.tuna.tsinghua.edu.cn/simple
+set -gx RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
+

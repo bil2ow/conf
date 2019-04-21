4bf96c1d2a1b5bd7217b1f684a4e27bdb3f212ad
billow <yaagnto@qq.com>
2019-04-21 23:22:12 +0800

    setup u

--- script/setup.sh
+++ script/setup.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
+# mirrorlist
+sudo sed -i.bak 's/^#\(Server.*tsinghua\)/\1/' /etc/pacman.d/mirrorlist
+# pacman
+sudo sed -i.bak '/^#\s*\[multilib\]/ {N; s/#\s*\(\[multilib\]\)\n#\s*\(Include.*\)/\1\n\2/}' /etc/pacman.conf
+
 sudo pacman -Syyu
-sudo pacman -S --noconfirm --needed base-devel \
+sudo pacman -S --noconfirm --needed \
+     base-devel \
+     os-prober grub efibootmgr \
+     gnupg sudo \
+     nvidia \
      rsync git mercurial aria2 \
      deepin deepin-screenshot deepin-terminal networkmanager \
      ibus ibus-libpinyin xclip \
+     firefox \
      ttf-fira-code adobe-source-han-sans-cn-fonts \
-     code \
+     vim emacs code \
+     aspell aspell-en \
      vulkan-devel \
      jdk-openjdk \
      nodejs npm yarn \
@@ -16,5 +27,38 @@ sudo pacman -S --noconfirm --needed base-devel \
      agda \
      stack \
      smlnj \
-     racket \
-     os-prober aspell aspell-en \
+     racket
+
+
+mkdir -p ~/tmp
+
+# conf  = dotfile
+git clone https://github.com/bil2ow/conf.git ~/tmp/conf
+cp -r ~/tmp/conf/{.,?}* ~/
+
+# yay
+git clone https://aur.archlinux.org/yay.git ~/tmp/yay
+cd ~/tmp/yay && makepkg -si --needed --noconfirm
+
+yay -S --norebuild --noredownload --notimeupdate --pgpfetch --noremovemake --needed \
+     optimus-manager jetbrains-toolbox update-grub
+
+# optimus-manager
+sudo systemctl start optimus-manager
+sudo optimus-manager --set-startup nvidia
+
+# spacemacs
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+
+# spacevim
+curl -sLf https://spacevim.org/cn/install.sh | bash
+
+rm -rf ~/tmp
+
+# Service
+systemctl --user enable emacs
+sudo systemctl enable lightdm
+sudo systemctl enable NetworkManager
+sudo systemctl enable optimus-manager
+
+sudo update-grub

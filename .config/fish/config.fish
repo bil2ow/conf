83088064855f05032cf488d5454a543fa5669e31
The Colour Out of Space <billow.fun@gmail.com>
2019-09-23 17:28:24 +0800

    add a typora keybind

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,12 +3,13 @@ abbr getclip "xclip -selection c -o"
 
 alias ec "emacsclient -c"
 alias et "emacsclient -t"
+alias en_anaconda "source /opt/anaconda/bin/activate root"
+alias de_anaconda "source /opt/anaconda/bin/deactivate root"
 
 alias proxy_off "set -e http_proxy;set -e https_proxy;set -e  ftp_proxy;set -e  all_proxy;set -e  HTTP_PROXY;set -e  HTTPS_PROXY;set -e  FTP_PROXY;set -e  ALL_PROXY"
 
-# copy from ~/.opam/opam-init/variables.fish but delete MANPATH line
-set -gx OPAM_SWITCH_PREFIX '/home/iov/.opam/default';
-set -gx CAML_LD_LIBRARY_PATH '/usr/lib/ocaml/stublibs:/usr/lib/ocaml';
-set -gx CAML_LD_LIBRARY_PATH '/home/iov/.opam/default/lib/stublibs':"$CAML_LD_LIBRARY_PATH";
-set -gx OCAML_TOPLEVEL_PATH '/home/iov/.opam/default/lib/toplevel';
-set -gx PATH '/home/iov/.opam/default/bin' $PATH;
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /opt/anaconda/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+

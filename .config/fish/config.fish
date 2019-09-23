15375752c30dd60fd3e56b8c3dc418a9dfba7034
The Colour Out of Space <billow.fun@gmail.com>
2019-09-23 10:57:56 +0800

    use gnome so use ibus~

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,11 @@ abbr getclip "xclip -selection c -o"
 alias ec "emacsclient -c"
 alias et "emacsclient -t"
 
-alias logout "i3-msg exit"
 alias proxy_off "set -e http_proxy;set -e https_proxy;set -e  ftp_proxy;set -e  all_proxy;set -e  HTTP_PROXY;set -e  HTTPS_PROXY;set -e  FTP_PROXY;set -e  ALL_PROXY"
 
-source /home/iov/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+# copy from ~/.opam/opam-init/variables.fish but delete MANPATH line
+set -gx OPAM_SWITCH_PREFIX '/home/iov/.opam/default';
+set -gx CAML_LD_LIBRARY_PATH '/usr/lib/ocaml/stublibs:/usr/lib/ocaml';
+set -gx CAML_LD_LIBRARY_PATH '/home/iov/.opam/default/lib/stublibs':"$CAML_LD_LIBRARY_PATH";
+set -gx OCAML_TOPLEVEL_PATH '/home/iov/.opam/default/lib/toplevel';
+set -gx PATH '/home/iov/.opam/default/bin' $PATH;

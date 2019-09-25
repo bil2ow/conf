b0b10d16f5a7e279675e5ab5d7eec8355c23a400
The Colour Out of Space <billow.fun@gmail.com>
2019-09-25 10:01:35 +0800

    fuck

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,13 +3,6 @@ abbr getclip "xclip -selection c -o"
 
 alias ec "emacsclient -c"
 alias et "emacsclient -t"
-alias en_anaconda "source /opt/anaconda/bin/activate root"
-alias de_anaconda "source /opt/anaconda/bin/deactivate root"
+alias conda_env "eval /opt/anaconda/bin/conda "shell.fish" "hook" $argv | source"
 
 alias proxy_off "set -e http_proxy;set -e https_proxy;set -e  ftp_proxy;set -e  all_proxy;set -e  HTTP_PROXY;set -e  HTTPS_PROXY;set -e  FTP_PROXY;set -e  ALL_PROXY"
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-eval /opt/anaconda/bin/conda "shell.fish" "hook" $argv | source
-# <<< conda initialize <<<
-
